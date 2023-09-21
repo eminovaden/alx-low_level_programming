c97dcfb73818e972b85f7646ff45f2d8c3fc9627
eminovaden <eminovaden@gmail.com>
2023-09-21 03:36:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,32 @@
 #include "main.h"
-/**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
- */
 
+/**
+ * cap_string - Capitalizes the first letter of each word in a string.
+ * @s: Input string.
+ * Return: Pointer to the updated string.
+ */
 char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
+	if (s[count] >= 'a' && s[count] <= 'z')
+		s[count] = s[count] - ('a' - 'A');
 	count++;
-	while (*(s + count) != '\0')
+
+	while (s[count] != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == sep_words[i])
+			if (s[count] == sep_words[i])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				if (s[count + 1] >= 'a' && s[count + 1] <= 'z')
+					s[count + 1] = s[count + 1] - ('a' - 'A');
 				break;
 			}
 		}
 		count++;
 	}
+
 	return (s);
 }
