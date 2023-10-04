7db9bc7d440dac0ab81189fa5cbc68be31de22c3
eminovaden <eminovaden@gmail.com>
2023-10-04 15:08:29 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 
 /**
  * *create_array - It creates an array of chars,
- * and will initializes it with a specific char
+ * and initializes it with a specific char
  * @size: size of the array to create
  * @c: char to initialize the array c
  * Return: pointer to the array (Success), NULL (Error)
