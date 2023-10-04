8fc52a8469d4cab620fd17565537a0291a1b5785
eminovaden <eminovaden@gmail.com>
2023-10-04 16:10:44 -0700

    message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,3 +8,24 @@
  * @c: char to initialize the array c
  * Return: pointer to the array (Success), NULL (Error)
  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *f;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	f = (char *) malloc(sizeof(char) * size);
+
+	if (f == NULL)
+		return (0);
+
+	while (i <= size - 1)
+	{
+		f[i] = c;
+		i++;
+	}
+	f[i] = 0;
+	return (f);
+}
