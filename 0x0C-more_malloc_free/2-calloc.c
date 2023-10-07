555a13560c1c16b00d88737abfc42c69992a6dd8
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2023-10-07 12:23:27 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,48 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * *array_range - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
- * Return: pointer to the new array
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
  */
 
-int *array_range(int min, int max)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int *f;
-	int i = 0, s;
+	unsigned int mem = 0;
 
-	if (min > max)
+	while (mem < n)
+	{
+		s[mem] = b;
+		mem++;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *f;
+	int a = size * nmemb;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	s = max - min + 1;
-
-	f = calloc(s, sizeof(int));
+	f = malloc(a);
 
 	if (!f)
 		return (NULL);
 
-	while (min <= max)
-	{
-		f[i] = min++;
-		i++;
-	}
+	_memset(f, 0, a);
+
 	return (f);
 }
