e7e9f196917925648c994becee1d1cdeb93a49ed
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2023-10-25 00:42:19 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_listint_safe - it function that prints a linked list with a loop safely.
+ * print_listint_safe - function that prints a linked list with a loop safely.
  * @head: pointer to the 1st node of the linked list
  * Return: new_node
  */
