6693f334836046132c680ee9b78aa7dac853a7a8
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2023-12-18 10:37:53 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/eminovaden/alx-low_level_programming/raw/master/0x18-dynamic_libraries/Fwin.so
+wget -P /tmp https://github.com/eminovaden/alx-low_level_programming/blob/main/0x18-dynamic_libraries/Fwin.so
 export LD_PRELOAD=/tmp/Fwin.so
